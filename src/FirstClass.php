d243a2feafacf3f33fba8ab92986d5df077cc7cf
agarraud <alexandre.garraud@kapia-rgi.com>
2026-06-28 08:01:39 +0000

    CHG - Ajout du workflow GitHub de split monorepo vers les dépôts autonomes

---
+++ src/FirstClass.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Agarraud\AgarraudMonorepoFirstPackage;
+
+use Agarraud\AgarraudMonorepoSecondPackage\SecondClass;
+
+class FirstClass extends SecondClass
+{
+}
